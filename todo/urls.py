e6639717f38cc45538a26ef02ef8bdd51912b012
Lucas Chen <lc20240328@gmail.com>
2025-05-03 01:22:44 +0800

    Edit and Delete Task Function

--- todo/urls.py
+++ todo/urls.py
@@ -2,6 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # Adding tasks
     path('addTask/', views.addTask, name='addTask'),
+    # Marking tasks as done and undone
     path('mark_as_done/<int:pk>/', views.mark_as_done, name='mark_as_done'),
+    path('mark_as_undone/<int:pk>/', views.mark_as_undone, name='mark_as_undone'),
+    # Editing tasks
+    path('edit_task/<int:pk>/', views.edit_task, name='edit_task'),
+    # Deleting tasks
+    path('delete_task/<int:pk>/', views.delete_task, name='delete_task'),
 ]
--- todo/views.py
+++ todo/views.py
@@ -14,3 +14,30 @@ def mark_as_done(request, pk):
     task.is_completed = True
     task.save()
     return redirect('todo')
+
+
+def mark_as_undone(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = False
+    task.save()
+    return redirect('todo')
+
+
+def edit_task(request, pk):
+    get_task = get_object_or_404(Task, pk=pk)
+    if request.method == 'POST':
+        new_task = request.POST['task']
+        get_task.task = new_task
+        get_task.save()
+        return redirect('todo')
+    else:
+        context = {
+            'get_task': get_task,
+        }
+        return render(request, 'edit_task.html', context)
+
+
+def delete_task(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.delete()
+    return redirect('todo')
